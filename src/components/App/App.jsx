fb2e0a434227fafe8bf6056bf003c90601c3c62d
Elizaveta Taran <lizataran103@gmail.com>
2023-09-20 17:51:16 +0300

    adjusted to linter test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Layout } from '../LayoutComponent/Layout.styled';
 import { ContactForm } from '../ContactForm/ContactForm';
 import { ContactList } from '../ContactList/ContactList';
